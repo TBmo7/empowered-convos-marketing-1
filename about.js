7300fb1a3767b535d118245d9210bbc37cbeb2cf
unknown <tbmoore85@gmail.com>
2020-01-09 22:27:10 -0500

    Final waiting for links for reg and signup

--- about.js
+++ about.js
@@ -33,3 +33,18 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
